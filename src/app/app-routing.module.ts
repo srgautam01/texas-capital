2429164b36f4690877ae9fdb17e969e1b3d801d3
sundar <sundar.gautam@selu.edu>
2021-10-31 12:55:28 -0500

    All html pages with appropriate input for images captures are defined.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { BusinessInfoComponent } from './pages/business-info/business-info.component';
+import { DisclosureVerificationComponent } from './pages/disclosure-verification/disclosure-verification.component';
+import { FundingDetailsComponent } from './pages/funding-details/funding-details.component';
+import { OwnerDetailsComponent } from './pages/owner-details/owner-details.component';
+import { SummaryDetailsComponent } from './pages/summary-details/summary-details.component';
+
+
+
+const routes: Routes = [
+  { path: 'page1', component: BusinessInfoComponent },
+  { path: 'page2', component: OwnerDetailsComponent },
+  { path: 'page3', component: DisclosureVerificationComponent },
+  { path: 'page4', component: FundingDetailsComponent },
+  { path: 'page5', component: SummaryDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
